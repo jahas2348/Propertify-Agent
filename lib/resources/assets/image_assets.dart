8cb620a1a176f5ae79f9cdbe13985ee53287ee4c
jahas2348 <jahaspty1@gmail.com>
2024-01-08 23:22:57 +0530

    Refactor: Optimize code structure and improve readability Optimized code and refactored several parts.

--- lib/resources/assets/image_assets.dart
+++ lib/resources/assets/image_assets.dart
@@ -1,6 +1,8 @@
 class ImageAssets {
   
   static const String welcomeImage = 'assets/images/welcome-image.png';
+  static const String welcomeImage2 = 'assets/images/welcome-image2.png';
+  static const String propertifylogo = 'assets/images/propertify1.jpg';
 
 
 }
